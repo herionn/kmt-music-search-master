65c2654fa3211731a95807106f1745d37e43c2d0
inv151ble <invisible.shizoid.ninja@gmail.com>
2014-05-11 20:57:55 +0300

    Getting started

---
+++ src/KMTArtist.java
@@ -0,0 +1,36 @@
+import de.umass.lastfm.Artist;
+import de.umass.lastfm.Caller;
+
+import java.text.DateFormat;
+import java.util.Collection;
+
+public class KMTArtist extends Artist{
+    //public static String Name;
+    public static String[] artists1 = new String[5];
+    public static Collection<KMTArtist> similar;
+    public static Collection<Artist> similar1;
+   /* public KMTArtist()
+    {
+        Collection<Artist> similar = Artist.getSimilar(KMTQuery.uArtName, 5, Main1.key);
+       int i=0; //Artist.getSimilar()
+        for (Artist artist : similar) {
+            artists1[i]=artist.getName();
+            i++;
+        }
+
+}*/
+
+    protected KMTArtist(Artist a) {
+        super(a.getName(), a.getUrl());
+                //this.getSimilar(); Artist.getSimilar(s,5,Main1.key);
+        int i=0; //Artist.getSimilar()
+        similar1=Artist.getSimilar(this.name, 5, Main1.key);
+        for (Artist artist : similar1) {
+            similar.add(new KMTArtist(artist));
+            artists1[i]=artist.getName();
+            i++;
+    }
+}
+
+
+}
---
+++ src/KMTResult.java
@@ -0,0 +1,9 @@
+
+public class KMTResult {
+    public KMTResult()
+    {  for(int i = 0;i<5; i++)
+     {
+        WindowFrame.userOut[i].setText(KMTQuery.testA.artists1[i]);
+     }
+    }
+}
